c42f98f9e82a6264f3dc4e59861ab0029287f5e5
Orville Daley <orvilledaley2004+git@gmail.com>
2022-07-05 20:22:18 -0500

    initial commit: setup

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async () => {
+  return { message: "Server is running... 😁" }
+})
+
+// Run the server
+try {
+  const port = 3001;
+  fastify.listen({ port });
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
